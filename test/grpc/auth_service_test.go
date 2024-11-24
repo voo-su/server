b8b1001e60449df57bcfc0cb43d160ef981f7141
Magomedcoder <info@magomedcoder.ru>
2024-11-24 20:33:00 +0300

    Feat: added test auth login service

---
+++ test/grpc/auth_service_test.go
@@ -0,0 +1,44 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"testing"
+	"time"
+	authPb "voo.su/api/grpc/pb"
+)
+
+func TestAuthLoginService(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	conn, err := grpc.NewClient("127.0.0.1:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Не удалось подключиться: %v", err)
+	}
+	defer conn.Close()
+
+	client := authPb.NewAuthServiceClient(conn)
+
+	res, err := client.Login(ctx, &authPb.AuthLoginRequest{
+		Email:    "test@test.test",
+		Platform: "android",
+	})
+	if err != nil {
+		t.Errorf("Ошибка при выполнении запроса Login: %v", err)
+		return
+	}
+
+	if res.Token == "" {
+		t.Error("Получен пустой токен, ожидался валидный токен")
+	}
+	if res.ExpiresIn == 0 {
+		t.Error("Поле ExpiresIn равно 0, ожидалось ненулевое время жизни токена")
+	}
+
+	fmt.Printf("Token: %s\n", res.Token)
+	fmt.Printf("Expires In: %d секунд\n", res.ExpiresIn)
+}
