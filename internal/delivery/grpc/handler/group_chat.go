40f4885da31ac57e01f40784a3344d95f68576b0
Magomedcoder <info@magomedcoder.ru>
2025-03-16 20:34:22 +0300

    Feat: implement CreateGroupChat, AddUserToGroupChat, RemoveUserFromGroupChat, and LeaveGroupChat methods in grpc

--- internal/delivery/grpc/handler/group_chat.go
+++ internal/delivery/grpc/handler/group_chat.go
@@ -2,10 +2,13 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"log"
 	groupChatPb "voo.su/api/grpc/gen/go/pb"
 	"voo.su/internal/config"
+	"voo.su/internal/constant"
 	"voo.su/internal/usecase"
 	"voo.su/pkg/grpcutil"
 	"voo.su/pkg/locale"
@@ -38,6 +41,21 @@ func NewGroupChatHandler(
 		GroupChatUseCase:       groupChatUseCase,
 		GroupChatMemberUseCase: groupChatMemberUseCase,
 	}
+}
+
+func (g *GroupChat) CreateGroupChat(ctx context.Context, in *groupChatPb.CreateGroupChatRequest) (*groupChatPb.CreateGroupChatResponse, error) {
+	uid := grpcutil.UserId(ctx)
+	gid, err := g.GroupChatUseCase.Create(ctx, &usecase.GroupCreateOpt{
+		UserId: uid,
+		Name:   in.Name,
+	})
+	if err != nil {
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("chat_creation_failed"))
+	}
+
+	return &groupChatPb.CreateGroupChatResponse{
+		Id: int64(gid),
+	}, nil
 }
 
 func (g *GroupChat) GetGroupChat(ctx context.Context, in *groupChatPb.GetGroupChatRequest) (*groupChatPb.GetGroupChatResponse, error) {
@@ -93,5 +111,88 @@ func (g *GroupChat) GetMembers(ctx context.Context, in *groupChatPb.GetMembersRe
 
 	return &groupChatPb.GetMembersResponse{
 		Items: items,
+	}, nil
+}
+
+func (g *GroupChat) AddUserToGroupChat(ctx context.Context, in *groupChatPb.AddUserToGroupChatRequest) (*groupChatPb.AddUserToGroupChatResponse, error) {
+	uid := grpcutil.UserId(ctx)
+
+	key := fmt.Sprintf("group-join:%d", in.Id)
+	if !g.GroupChatUseCase.RedisLockCacheRepo.Lock(ctx, key, 20) {
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("network_error"))
+	}
+
+	defer g.GroupChatUseCase.RedisLockCacheRepo.UnLock(ctx, key)
+
+	group, err := g.GroupChatUseCase.GroupChatRepo.FindById(ctx, int(in.Id))
+	if err != nil {
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("network_error"))
+	}
+
+	if group != nil && group.IsDismiss == 1 {
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("group_dissolved"))
+	}
+
+	var uids []int
+	for _, id := range in.UserIds {
+		uids = append(uids, int(id))
+	}
+
+	if len(uids) == 0 {
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("invited_friends_empty"))
+	}
+
+	if !g.GroupChatMemberUseCase.MemberRepo.IsMember(ctx, int(in.Id), uid, true) {
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("not_group_member_invite"))
+	}
+
+	if err := g.GroupChatUseCase.Invite(ctx, &usecase.GroupInviteOpt{
+		UserId:    uid,
+		GroupId:   int(in.Id),
+		MemberIds: uids,
+	}); err != nil {
+		log.Println(err)
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("invite_friends_failed"))
+	}
+
+	return &groupChatPb.AddUserToGroupChatResponse{}, nil
+}
+
+func (g *GroupChat) RemoveUserFromGroupChat(ctx context.Context, in *groupChatPb.RemoveUserFromGroupChatRequest) (*groupChatPb.RemoveUserFromGroupChatResponse, error) {
+	uid := grpcutil.UserId(ctx)
+	if !g.GroupChatMemberUseCase.MemberRepo.IsLeader(ctx, int(in.Id), uid) {
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("no_permission_for_action"))
+	}
+	var uids []int
+	uids = append(uids, int(in.UserId))
+
+	if err := g.GroupChatUseCase.RemoveMember(ctx, &usecase.GroupRemoveMembersOpt{
+		UserId:    uid,
+		GroupId:   int(in.Id),
+		MemberIds: uids,
+	}); err != nil {
+		log.Println(err)
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("network_error"))
+	}
+
+	return &groupChatPb.RemoveUserFromGroupChatResponse{
+		Success: true,
+	}, nil
+}
+
+func (g *GroupChat) LeaveGroupChat(ctx context.Context, in *groupChatPb.LeaveGroupChatRequest) (*groupChatPb.LeaveGroupChatResponse, error) {
+	uid := grpcutil.UserId(ctx)
+	if err := g.GroupChatUseCase.Secede(ctx, int(in.Id), uid); err != nil {
+		log.Println(err)
+		return nil, status.Error(codes.Unknown, g.Locale.Localize("network_error"))
+	}
+
+	sid := g.ChatUseCase.ChatRepo.FindBySessionId(uid, int(in.Id), constant.ChatGroupMode)
+	if err := g.ChatUseCase.Delete(ctx, uid, sid); err != nil {
+		log.Println(err)
+	}
+
+	return &groupChatPb.LeaveGroupChatResponse{
+		Success: true,
 	}, nil
 }
